5257c29e2f54a8efa2ab32027cb0b4251af4301d
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-06-10 11:49:09 +0800

    check time conflict

--- helper.js
+++ helper.js
@@ -1,10 +1,9 @@
 javascript:
 (function(){
 
-var day = ["一", "二", "三", "四", "五", "六", "日"];
-var selectrows = document.getElementsByClassName("x-grid3-row-selected");
+var tt = document.getElementsByClassName("x-grid3-body")[document.getElementsByClassName("x-grid3-body").length-1];
+var allrows = document.getElementsByClassName("x-grid3-row");
 
-var tt = document.body.children[4].children[6].children[1].children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[1].children[1].children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[1].children[0];
 function init() {
 	for (var i = tt.children.length - 1; i >= 0; i--) {
 		for (var j = tt.children[i].children[0].children[0].children[0].children.length - 1; j >= 2; j--) {
@@ -24,9 +23,15 @@ function clear() {
 			}
 		}
 	}
+	for (var j = 0; j < allrows.length; j++) {
+		allrows[j].children[0].style.background = "";
+	}
 }
 function update() {
 	clear();
+
+	var day = ["一", "二", "三", "四", "五", "六", "日"];
+	var selectrows = document.getElementsByClassName("x-grid3-row-selected");
 	for (var i = selectrows.length - 1; i >= 0; i--) {
 		var cla = selectrows[i].children[0].children[0].children[0];
 		var name = cla.children[2].innerText.trim();
@@ -44,6 +49,32 @@ function update() {
 		for (var j = times; j <= timee; j++) {
 			tt.children[j].children[0].children[0].children[0].children[timed].children[0].style.color = "red";
 			tt.children[j].children[0].children[0].children[0].children[timed].children[0].innerHTML += name+"<br>";
+		}
+		for (var j = 0; j < allrows.length; j++) {
+			if (selectrows[i] == allrows[j]) {
+				continue;
+			}
+			if (!allrows[j].children[0].children[0].children[0].children[0].children[0].classList.contains("x-grid3-col-checker")) {
+				console.log("break");
+				break;
+			}
+			var timestr = allrows[j].children[0].children[0].children[0].children[10].children[0].innerText;
+			var time = timestr.match(/\((.)\)(\d)-(\d)/);
+			if (time === null) {
+				continue;
+			}
+			var timed2 = +day.indexOf(time[1])+2;
+			var times2 = +time[2];
+			var timee2 = +time[3];
+			if (times2 >= 5) {
+				times2 += 1;
+			}
+			if (timee2 >= 5) {
+				timee2 += 1;
+			}
+			if (timed == timed2 && !(timee < times2 || times > timee2)) {
+				allrows[j].children[0].style.background = "#ffcccc";
+			}
 		}
 	}
 }
